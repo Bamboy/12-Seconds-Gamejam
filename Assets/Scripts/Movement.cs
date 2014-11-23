b11bd90ed3e1870da445508b01d41f20c2b2f08a
Stephan Ennen <bamboy360@gmail.com>
2014-11-23 15:29:57 -0800

    Stuff

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -2,31 +2,32 @@
 using System.Collections;
 
 public class Movement : MonoBehaviour {
-	public GameObject lanes;
+	public float laneWidth = 1.0f;
+	//public GameObject lanes;
 	private GameObject[] lane;
 	private Rigidbody character;
 	private Camera mainCam;
 	private bool jumped;
-	private float distToGround;
+	//private float distToGround;
 	private int laneNumber;
 	private Vector3 charPos;
-	private Timer timer;
+	//private Timer timer;
 	private PlayerUnit pu;
 
 	private void Awake(){
 		character = GetComponent<Rigidbody>();
 		lane = GameObject.FindGameObjectsWithTag("Lanes");
-		timer = gameObject.AddComponent<Timer>();
+		//timer = gameObject.AddComponent<Timer>();
 		pu = GetComponent<PlayerUnit>();
 		mainCam = Camera.main;
-		timer.Init(10);
+		//timer.Init(10);
 	}
-	private void Start(){
+	/*private void Start(){
 		distToGround = GetComponentInChildren<BoxCollider>().bounds.extents.y;
-	}
+	}*/
 	private void Update(){
 		if(Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow)){
-			if(laneNumber < lane.Length){
+			if(laneNumber < lane.Length - 1){
 				laneNumber++;
 			}
 		} else if(Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow)){
@@ -35,7 +36,8 @@ public class Movement : MonoBehaviour {
 			}
 		}
 		charPos = this.transform.position;
-		charPos.z = lane[laneNumber].transform.position.z;
+		//charPos.z = lane[laneNumber].transform.position.z;
+		charPos.z = laneNumber * laneWidth;
 		this.transform.position = charPos;
 	}
 	private void FixedUpdate(){
---
+++ Assets/Scripts/Player/PlyMovement.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlyMovement : MonoBehaviour {
+	public int laneCount = 4;
+	public float laneWidth = 2.5f;
+	public float speed = 1.0f;
+	private int laneNumber;
+
+
+	void Update()
+	{
+		if(Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow)){
+			if(laneNumber < laneCount - 1){
+				laneNumber++;
+			}
+		} else if(Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow)){
+			if(laneNumber > 0){
+				laneNumber--;
+			}
+		}
+		Vector3 t = transform.position;
+		t.z = laneNumber * laneWidth;
+		transform.position = t;
+
+		transform.Translate( -1 * speed * Time.deltaTime, 0, 0, Space.World );
+	}
+
+
+}
