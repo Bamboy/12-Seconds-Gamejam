3cacdf3f69c139ad5121b5e5c20adccb27bf2643
pertinate <ppertinate@gmail.com>
2014-11-23 21:07:26 -0600

    trololololhuehuehuehue

---
+++ Assets/Scripts/Player/EnemyDetection.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyDetection : MonoBehaviour {
+	void Update () {
+		foreach(GameObject go in GameObject.FindGameObjectsWithTag("Crab")){
+			if(Vector3.Distance(transform.position, go.transform.position) < 2.0f){
+				BaseTimer.instance.TimeModifier -= 10;
+				Destroy(go);
+			}
+		}
+		foreach(GameObject go in GameObject.FindGameObjectsWithTag("Starfish")){
+			if(Vector3.Distance(transform.position, go.transform.position) < 2.0f){
+				BaseTimer.instance.TimeModifier -= 10;
+				Destroy(go);
+			}
+		}
+		foreach(GameObject go in GameObject.FindGameObjectsWithTag("Plant")){
+			if(Vector3.Distance(transform.position, go.transform.position) < 2.0f){
+				BaseTimer.instance.TimeModifier -= 10;
+				Destroy(go);
+			}
+		}
+	}
+}
