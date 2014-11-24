992427e50ea0b449783f24f51f9ab5e32007d943
Stephan Ennen <bamboy360@gmail.com>
2014-11-23 17:15:22 -0800

    even moar!

---
+++ Assets/Scripts/Player/Projectile.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class Projectile : MonoBehaviour 
+{
+	public Vector3 direction;
+	public float speed = 3.0f;
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		transform.Translate( direction * speed * Time.deltaTime );
+	}
+
+
+}
