bc98faad9432296dd7b1f5ac70600d1e254715fb
Cristian Vozoca <vozochris@gmail.com>
2014-11-29 21:51:03 +0200

    Added Music player

---
+++ Assets/Scripts/Main.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+//By Cristian "vozochris" Vozoca
+public class Main : MonoBehaviour
+{
+	public static MusicPlayer audioPlayer;
+
+	private void Awake()
+	{
+		AudioHelper.MusicVolume = 0.1f;
+		audioPlayer = gameObject.AddComponent<MusicPlayer>();
+	}
+}
