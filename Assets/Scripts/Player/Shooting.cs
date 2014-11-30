e48f3fe04cb6b91c7b4f7eb966b72a3101de908d
Stephan Ennen <bamboy360@gmail.com>
2014-11-30 08:37:42 -0800

    Reverted shooting.cs

--- Assets/Scripts/Player/Shooting.cs
+++ Assets/Scripts/Player/Shooting.cs
@@ -3,14 +3,10 @@ using System.Collections;
 
 public class Shooting : MonoBehaviour 
 {
-	/*public GameObject projectile;
+	public GameObject projectile;
 	public float shootDelay = 0.75f;
 	private bool countdown;
-<<<<<<< HEAD
 	private float timer;
-=======
-	private float timer = 0.5f;
->>>>>>> 76181d48364538cb178a98a20221c7cd9b821813
 
 	//public Transform target;
 	public float maxAngle = 35.0f;
@@ -39,27 +35,15 @@ public class Shooting : MonoBehaviour
 		if( Input.GetMouseButtonDown(0) && !countdown )
 		{
 			countdown = true;
-<<<<<<< HEAD
 			timer = shootDelay;
 			Shoot ();
-=======
-			timer = 0.5f;
-			RaycastHit hit;
-			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-			if (Physics.Raycast(ray, out hit))
-			{
-				//hit.collider.renderer.material.color = Color.red;
-			}
-			else
-				Debug.LogError ("Nothing was hit!", this);
 
->>>>>>> 76181d48364538cb178a98a20221c7cd9b821813
-
+			/*
 			Vector2 spawnPos = VectorExtras.OffsetPosInPointDirection( new Vector2(transform.position.x, transform.position.z), new Vector2(hit.point.x, hit.point.z), 0.6f );
 			GameObject proj = (GameObject)Instantiate( projectile, new Vector3( spawnPos.x, 1.0f, spawnPos.y ), Quaternion.identity );
 			Vector2 dir = VectorExtras.Direction( new Vector2(transform.position.x, transform.position.z), new Vector2(hit.point.x, hit.point.z) );
 			proj.GetComponent<Projectile>().direction = new Vector3( dir.x, 0.0f, dir.y );
-			proj.GetComponent<Projectile>().speed += PlyMovement.speed;
+			proj.GetComponent<Projectile>().speed += PlyMovement.speed; */
 		}
 	}
 	void Shoot()
@@ -104,5 +88,5 @@ public class Shooting : MonoBehaviour
 			Debug.LogError ("Nothing was hit!", this);
 
 		LookAtTarget( hit.point );
-	}*/
+	}
 }
