4f303f7c9f25e1d10d84ca17516f551e1842731f
pertinate <ppertinate@gmail.com>
2014-12-08 23:58:04 -0600

    Update GUI

--- Assets/Scripts/GUI/MainMenu.cs
+++ Assets/Scripts/GUI/MainMenu.cs
@@ -2,22 +2,42 @@
 using System.Collections;
 
 public class MainMenu : MonoBehaviour {
+	public Texture background;
+	public Texture boxes;
+	public Texture mainScreenTexture;
+	public Texture play;
+	public Texture credits;
+	public Texture quit;
+	float Width = 800;
+	float Height = 480;
+	Vector3 Scale;
 	bool creditToggle;
 
 	void OnGUI(){
+		Scale.x = Screen.width/Width;
+		Scale.y = Screen.height/Height;
+		Scale.z = 1;
+		GUI.matrix = Matrix4x4.TRS(Vector3.zero, Quaternion.identity, Scale);
+		GUI.DrawTexture(new Rect(0, 0, Width, Height), background);
+		GUI.DrawTexture(new Rect((Width/2) - 125, (Height/2)- 175, 250, 350), boxes);
+		GUI.DrawTexture(new Rect((Width/2) - 100, (Height/2) - 145, 200, 70), mainScreenTexture);
+		if(Application.isLoadingLevel)
+			GUI.Label(new Rect(0, 0, 100, 50), "Loading...");
 		Play();
 		Credit();
 		Quit();
 	}
 	void Play(){
-		if(GUI.Button(new Rect(0, 0, 100, 100), "Play")){
+		if(GUI.Button(new Rect((Width/2) - 88, (Height/2) - 30, 170, 30), "", GUIStyle.none)){
 			Application.LoadLevel(2);
 		}
+		GUI.DrawTexture(new Rect((Width/2) - 50, (Height/2) - 22.5f, 100, 20), play);
 	}
 	void Credit(){
-		if(GUI.Button(new Rect(0, 100, 100, 100), "Credits")){
+		if(GUI.Button(new Rect((Width/2) - 88, (Height/2) + 57.5f, 170, 30), "", GUIStyle.none)){
 			creditToggle = !creditToggle;
 		}
+		GUI.DrawTexture(new Rect((Width/2) - 60, (Height/2) + 54, 120, 20), credits);
 		if(creditToggle){
 			GUI.Label(new Rect(150, 0, 200, 100), "Justin Bruystens");
 			GUI.Label(new Rect(150, 50, 200, 100), "Nick Evans");
@@ -31,8 +51,9 @@ public class MainMenu : MonoBehaviour {
 		}
 	}
 	void Quit(){
-		if(GUI.Button(new Rect(0, 200, 100, 100), "Quit")){
+		if(GUI.Button(new Rect((Width/2) - 88, (Height/2) + 145, 170, 30), "", GUIStyle.none)){
 			Application.Quit();
 		}
+		GUI.DrawTexture(new Rect((Width/2) - 60, (Height/2) + 135, 120, 20), quit);
 	}
 }
--- Assets/Scripts/GUI/PauseMenu.cs
+++ Assets/Scripts/GUI/PauseMenu.cs
@@ -2,9 +2,13 @@
 using Utils.Audio;
 
 public class PauseMenu : MonoBehaviour {
+	public GUISkin skin;
+	public GUISkin pauseButton;
+	public Texture2D[] textures;
 	private bool isPaused;
 	private bool pauseAll;
 	private bool temporarybool;
+
 	private void Update (){
 		if(isPaused){
 			Time.timeScale = 0.0f;
@@ -18,19 +22,27 @@ public class PauseMenu : MonoBehaviour {
 	}
 	private void OnGUI(){
 		if(!isPaused){
+			GUI.skin = pauseButton;
 			if(GUI.Button(new Rect(Screen.width - 50, 0, 50, 50), "")){
 				isPaused = true;
 			}
 		}
+		GUI.skin = skin;
 		if(isPaused){
-			GUI.BeginGroup(new Rect((Screen.width/2) - 75, (Screen.height/2) - 59, 150, 118));
-			GUI.Box(new Rect(0, 0, 150, 118), "Paused");
-			if(GUI.Button(new Rect(0, 18, 150, 50), "Continue")){
+			GUI.BeginGroup(new Rect((Screen.width/2) - 150, (Screen.height/2) - 150, 300, 300));
+			GUI.Box(new Rect(0, 0, 300, 300), "Paused");
+			if(GUI.Button(new Rect(50, 75, 200, 60), "")){
 				isPaused = false;
 			}
-			if(GUI.Button(new Rect(0, 68, 150, 50), "Quit")){
+			GUI.DrawTexture(new Rect(92.5f, 87.5f, 105, 27.5f), textures[0]);//Resume
+			if(GUI.Button(new Rect(50, 135, 200, 60), "")){
+				Application.LoadLevel(Application.loadedLevel);
+			}
+			GUI.DrawTexture(new Rect(92.5f, 147.5f, 105, 27.5f), textures[1]);
+			if(GUI.Button(new Rect(50, 195, 200, 60), "")){
 				Application.LoadLevel(1);
 			}
+			GUI.DrawTexture(new Rect(92.5f, 207.5f, 105, 27.5f), textures[2]);
 			GUI.EndGroup();
 		}
 	}
