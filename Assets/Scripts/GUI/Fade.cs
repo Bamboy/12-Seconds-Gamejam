b1151c4647755a1c908ad9c1e04108e28a5f5ea2
Stephan Ennen <bamboy360@gmail.com>
2014-12-15 11:19:08 -0800

    GUI fading

---
+++ Assets/Scripts/GUI/Fade.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Excelsion.Gooey
+{
+	public class Fade : MonoBehaviour 
+	{
+		public float time = 3.0f;
+		public Gradient gradient;
+		private Texture2D fade;
+		
+		private void Start()
+		{
+			fade = new Texture2D(1,1);
+		}
+		
+		private void OnGUI()
+		{	
+			SetFade( Time.timeSinceLevelLoad / time );
+			GUI.DrawTexture(new Rect(0, 0, Screen.width, Screen.height), fade, ScaleMode.StretchToFill);
+		}
+		
+		private void SetFade( float t )
+		{
+			fade.SetPixel(0,0, gradient.Evaluate( t ));
+			fade.Apply();
+		}
+	}
+}
--- Assets/Scripts/GUI/SplashScreen.cs
+++ Assets/Scripts/GUI/SplashScreen.cs
@@ -1,17 +1,37 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-namespace asfgsadf{
-	public class SplashScreen : MonoBehaviour {
+namespace Excelsion.Gooey
+{
+	public class SplashScreen : MonoBehaviour 
+	{
 		public Texture2D SplashBackground;
-
-		private void Start(){
-			Invoke("TriggerNextScene", 2f);
+		public float splashTime = 3.0f;
+		public Gradient gradient;
+		private Texture2D fade;
+		
+		private void Start()
+		{
+			fade = new Texture2D(1,1);
+			Invoke("TriggerNextScene", splashTime);
 		}
-		private void OnGUI(){
-			GUI.DrawTexture(new Rect(0, 0, Screen.width, Screen.height), SplashBackground);
+		
+		private void OnGUI()
+		{	
+			//We need to make sure that the logo draws FIRST so that the fade effect will draw over it. This is why we don't attach a Fade.cs,
+			//because you cant guarentee which will render first.
+			GUI.DrawTexture(new Rect(0, 0, Screen.width, Screen.height), SplashBackground, ScaleMode.ScaleToFit);
+			SetFade( Time.timeSinceLevelLoad / splashTime );
+			GUI.DrawTexture(new Rect(0, 0, Screen.width, Screen.height), fade, ScaleMode.StretchToFill);
 		}
-		private void TriggerNextScene(){
+		
+		private void SetFade( float t )
+		{
+			fade.SetPixel(0,0, gradient.Evaluate( t ));
+			fade.Apply();
+		}
+		private void TriggerNextScene()
+		{
 			Application.LoadLevel(1);
 		}
 	}
