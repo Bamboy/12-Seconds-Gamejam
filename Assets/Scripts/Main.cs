badac17ac306f115d7a45970937dca812166535a
pertinate <ppertinate@gmail.com>
2014-12-27 19:31:33 -0600

    Fixed the saving with audio variables. Redid the main menu for pc.

--- Assets/Scripts/Main.cs
+++ Assets/Scripts/Main.cs
@@ -3,7 +3,7 @@ using Utils.Audio;
 using UnityEngine.UI;
 using Enemies;
 
-//By Cristian "vozochris" Vozoca
+//By Cristian "vozochris" Vozoca and Nick Evans
 public class Main : MonoBehaviour
 {
 	public static Main instance;
@@ -15,27 +15,27 @@ public class Main : MonoBehaviour
 
 	private bool playerAlive = true;
 
-	private void Awake()
+	private void Start()
 	{
 		instance = this;
 
 		Time.timeScale = 1.0f;
-		if(Application.loadedLevel == 1){
-			if(PlayerPrefs.HasKey("firstStart")){
-				UI.SliderFunctions.instance.volumeSliders[0].value = PlayerPrefs.GetFloat("masterVolume");
-				UI.SliderFunctions.instance.volumeSliders[1].value = PlayerPrefs.GetFloat("musicVolume");
-				UI.SliderFunctions.instance.volumeSliders[2].value = PlayerPrefs.GetFloat("effectVolume");
-				UI.SliderFunctions.instance.volumeSliders[3].value = PlayerPrefs.GetFloat("voiceVolume");
+		if(Application.loadedLevel == 1 || Application.loadedLevel == 2){
+			if(PlayerPrefs.HasKey("hasSet")){
+				AudioHelper.MasterVolume = PlayerPrefs.GetFloat("masterVolume");
+				AudioHelper.MusicVolume = PlayerPrefs.GetFloat("musicVolume");
+				AudioHelper.EffectVolume = PlayerPrefs.GetFloat("effectVolume");
+				AudioHelper.VoiceVolume = PlayerPrefs.GetFloat("voiceVolume");
 			} else {
 				AudioHelper.MasterVolume = 1f;
 				AudioHelper.MusicVolume = 1f;
 				AudioHelper.EffectVolume = 1f;
 				AudioHelper.VoiceVolume = 1f;
 			}
-			AudioHelper.MasterVolume = UI.SliderFunctions.instance._masterVolume;
-			AudioHelper.MusicVolume = UI.SliderFunctions.instance._musicVolume;
-			AudioHelper.EffectVolume = UI.SliderFunctions.instance._effectVolume;
-			AudioHelper.VoiceVolume = UI.SliderFunctions.instance._voiceVolume;
+			UI.SliderFunctions.instance.volumeSliders[0].value = AudioHelper.MasterVolume;
+			UI.SliderFunctions.instance.volumeSliders[2].value = AudioHelper.MusicVolume;
+			UI.SliderFunctions.instance.volumeSliders[1].value = AudioHelper.EffectVolume;
+			UI.SliderFunctions.instance.volumeSliders[3].value = AudioHelper.VoiceVolume;
 		}
 		if(Application.loadedLevel == 3){
 			player = GameObject.FindGameObjectWithTag("Player").transform;
@@ -58,14 +58,14 @@ public class Main : MonoBehaviour
 		PlayerPrefs.SetFloat("musicVolume", AudioHelper.MusicVolume);
 		PlayerPrefs.SetFloat("effectVolume", AudioHelper.EffectVolume);
 		PlayerPrefs.SetFloat("voiceVolume", AudioHelper.VoiceVolume);
-		PlayerPrefs.SetString("firstStart", "false");
+		PlayerPrefs.SetString("hasSet", "true");
 	}
 	private void OnApplicationQuit(){
 		PlayerPrefs.SetFloat("masterVolume", AudioHelper.MasterVolume);
 		PlayerPrefs.SetFloat("musicVolume", AudioHelper.MusicVolume);
 		PlayerPrefs.SetFloat("effectVolume", AudioHelper.EffectVolume);
 		PlayerPrefs.SetFloat("voiceVolume", AudioHelper.VoiceVolume);
-		PlayerPrefs.SetString("firstStart", "false");
+		PlayerPrefs.SetString("hasSet", "true");
 	}
 
 	public static bool PlayerAlive
