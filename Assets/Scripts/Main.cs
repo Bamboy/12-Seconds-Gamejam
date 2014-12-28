54281e2810327a415e7db51609fc4ef91967646f
Stephan Ennen <bamboy360@gmail.com>
2014-12-27 18:32:26 -0800

    wtf BOOM

--- Assets/Scripts/Main.cs
+++ Assets/Scripts/Main.cs
@@ -20,30 +20,19 @@ public class Main : MonoBehaviour
 		instance = this;
 
 		Time.timeScale = 1.0f;
-		if(Application.loadedLevel == 1 || Application.loadedLevel == 2){
-<<<<<<< HEAD
+		if(Application.loadedLevel == 1 || Application.loadedLevel == 2)
+		{
 			if(PlayerPrefs.HasKey("hasSet")){
 				AudioHelper.MasterVolume = PlayerPrefs.GetFloat("masterVolume");
 				AudioHelper.MusicVolume = PlayerPrefs.GetFloat("musicVolume");
 				AudioHelper.EffectVolume = PlayerPrefs.GetFloat("effectVolume");
 				AudioHelper.VoiceVolume = PlayerPrefs.GetFloat("voiceVolume");
-=======
-			if(PlayerPrefs.HasKey("firstStart")){
-				UI.SliderFunctions.instance.volumeSliders[0].value = PlayerPrefs.GetFloat("masterVolume");
-				UI.SliderFunctions.instance.volumeSliders[1].value = PlayerPrefs.GetFloat("musicVolume");
-				UI.SliderFunctions.instance.volumeSliders[2].value = PlayerPrefs.GetFloat("effectVolume");
-				UI.SliderFunctions.instance.volumeSliders[3].value = PlayerPrefs.GetFloat("voiceVolume");
->>>>>>> 5b569b6cebcd682bb6a830114b6419e084347031
 			} else {
 				AudioHelper.MasterVolume = 1f;
 				AudioHelper.MusicVolume = 1f;
 				AudioHelper.EffectVolume = 1f;
 				AudioHelper.VoiceVolume = 1f;
 			}
-			UI.SliderFunctions.instance.volumeSliders[0].value = AudioHelper.MasterVolume;
-			UI.SliderFunctions.instance.volumeSliders[2].value = AudioHelper.MusicVolume;
-			UI.SliderFunctions.instance.volumeSliders[1].value = AudioHelper.EffectVolume;
-			UI.SliderFunctions.instance.volumeSliders[3].value = AudioHelper.VoiceVolume;
 		}
 		if(Application.loadedLevel == 3){
 			player = GameObject.FindGameObjectWithTag("Player").transform;
